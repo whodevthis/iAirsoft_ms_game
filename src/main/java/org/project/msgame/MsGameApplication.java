7520d6c67b319f87ce567cfa6b6f6c73f74193ad
MiguelMerchan <miguel.merrec@gmail.com>
2026-05-05 19:25:35 +0200

    First Ccmmit

---
+++ src/main/java/org/project/msgame/MsGameApplication.java
@@ -0,0 +1,13 @@
+package org.project.msgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/msgame/MsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.msgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
