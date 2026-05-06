be8b333262292da7ddd18c725afe2778ebf09664
MiguelMerchan <miguel.merrec@gmail.com>
2026-05-06 21:59:43 +0200

    feature: add TeamFlag(VO)

--- src/main/java/Domain/valueObjects/TeamFlag.java
+++ src/main/java/Domain/valueObjects/TeamFlag.java
@@ -1,10 +1,9 @@
 package Domain.valueObjects;
 
-
 import org.springframework.util.Assert;
 
-public record PatternCammo(String imagePath, String name) {
-    public PatternCammo {
+public record TeamFlag(String imagePath, String name) {
+    public TeamFlag {
         Assert.hasText(name, "name must not be blank");
         if (imagePath != null) {
             Assert.hasText(imagePath, "imagePath must not be blank if provided");
